da4ed06c42241decd4e5c4a9f1fcfed8e98ae38d
bplanica <bplanica@uw.edu>
2020-06-29 16:43:41 -0500

    flask-mailroom assignment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donor, Donation
 
 app = Flask(__name__)
 
@@ -11,13 +11,31 @@ app = Flask(__name__)
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/donations/add', methods=['GET', 'POST'])
+def add():
+
+    if request.method == 'POST':
+        try:
+            donor = Donor.select().where(Donor.name == request.form['donor']).get()
+            donation = Donation(donor=donor, value=int(request.form['amount']))
+            donation.save()
+
+            return redirect(url_for('all'))
+
+        except Donor.DoesNotExist:
+            return render_template('add.jinja2', error="Donor not found.")
+
+    else:
+        return render_template('add.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
